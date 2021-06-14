e5fda2af3c3b15b9f1573698c13ea62c64c3d8fb
aarongermaine <vcnts0@gmail.com>
2021-06-14 16:07:32 -0600

    ughd

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ mongoose.connect(
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/public/index.html"));
 });
 
 app.listen(PORT, () => console.log(`🌎 ==> API server now on port ${PORT}!`));
